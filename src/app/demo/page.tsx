d35336bb09e9070f7d41f4fcfb3d25eb93379146
杨青峰 <>
2024-11-07 18:03:21 +0800

    demo file

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { uInput } from 'udes1gn-ui'
+
+export default function page() {
+    return (
+        <div>
+            <div>demo page</div>
+            <uInput />
+        </div>
+    )
+}
